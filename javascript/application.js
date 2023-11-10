bd030f1d609393bcef623581882030c70a8d97fd
lin MA <lin.ma@ieseg.fr>
2023-11-10 22:20:34 +0100

    Using Vue.js

--- javascript/application.js
+++ javascript/application.js
@@ -1,14 +1,9 @@
-import Mustache from "mustachejs";
+import { createApp } from "vue";
 
-console.log("hello from application.js")
-
-const movieContainer = document.querySelector("#results");
-const template = document.querySelector("#movieCardTemplate").innerHTML;
-
-fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
-  .then(response => response.json())
-  .then((data) => {
-    const movieData = { "movies": data.Search };
-    const output = Mustache.render(template,movieData);
-    movieContainer.innerHTML = output;
-  })
+createApp({
+  data(){
+    return{
+      message: "Hello from Vue.js"
+    }
+  }
+}).mount("#results");
