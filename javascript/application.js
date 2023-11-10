f1c54b21c18b5081c889d1db78a04db72a9a6b15
lin MA <lin.ma@ieseg.fr>
2023-11-10 19:30:15 +0100

    Mustache use

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,14 @@
+import Mustache from "mustachejs";
+
 console.log("hello from application.js")
+
+const movieContainer = document.querySelector("#results");
+const template = document.querySelector("#movieCardTemplate").innerHTML;
+
+fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+  .then(response => response.json())
+  .then((data) => {
+    const movieData = { "movies": data.Search };
+    const output = Mustache.render(template,movieData);
+    movieContainer.innerHTML = output;
+  })
